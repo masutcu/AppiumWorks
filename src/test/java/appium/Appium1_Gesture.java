ef0563ec7a9462ca8f8701c85f565e835b1782ad
masutcu <masutcu@gmail.com>
2023-07-28 23:25:36 +0300

    v1

--- src/test/java/appium/Appium1_Gesture.java
+++ src/test/java/appium/Appium1_Gesture.java
@@ -17,21 +17,6 @@ public class Appium1_Gesture {
 
     @Test
     public void test() throws MalformedURLException {
-         /*DesiredCapabilities desiredCap=new DesiredCapabilities();
-
-        UiAutomator2Options options=new UiAutomator2Options();
-        options.setPlatformName("Android");
-        options.setAutomationName(AutomationName.ANDROID_UIAUTOMATOR2);
-        options.setDeviceName("masutcu");
-        options.setApp(System.getProperty("user.dir")+"src/Apps/GestureTool.apk");
-
-
-        options.setCapability("noReset", true);
-
-        AndroidDriver driver=new AndroidDriver(new URL("http://127.0.0.1:4723"), options);
-*/
-
-        //com.google.android.apps.nexuslauncher/com.google.android.apps.nexuslauncher.NexusLauncherActivity
 
         //Appium1 sürüm ayarları
         DesiredCapabilities capabilities=new DesiredCapabilities();
@@ -40,24 +25,23 @@ public class Appium1_Gesture {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");//appiumdan gelen
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, AutomationName.ANDROID_UIAUTOMATOR2);
         capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "masutcu");
-        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
-        capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
-        capabilities.setCapability("appPackage", "com.android.chrome");
-        capabilities.setCapability("appActivity", "com.google.android.apps.chrome.Main");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "13");
+        //capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+        capabilities.setCapability("appPackage", "com.android.settings");
+        capabilities.setCapability("appActivity", "com.android.settings.Settings");
 
 
-
-
-        // Appium server URL
-        URL url = new URL("http://127.0.0.1:4723/wd/hub");
 
         // Initialize AndroidDriver
-        AndroidDriver driver = new AndroidDriver(url, capabilities);
-        // Navigate to Amazon.com.tr
-        driver.get("https://www.amazon.com.tr");
+        AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
 
 
-        //capabilities.setCapability("app", "C:\\Users\\Lenovo\\IdeaProjects\\Appium\\src\\Apps\\Gesture Tool_1.3_Apkpure.apk");
+
+
+
+
+
+
 
     }
 
