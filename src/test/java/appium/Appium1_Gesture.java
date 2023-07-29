91a6918ae5c424acd89bc3576baeca47ed0dd8ec
masutcu <masutcu@gmail.com>
2023-07-29 14:54:25 +0300

    ilktest

--- src/test/java/appium/Appium1_Gesture.java
+++ src/test/java/appium/Appium1_Gesture.java
@@ -7,7 +7,10 @@ import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.options.UiAutomator2Options;
 import io.appium.java_client.remote.AutomationName;
 import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -28,14 +31,34 @@ public class Appium1_Gesture {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10");
         capabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\Lenovo\\IdeaProjects\\Appium\\src\\Apps\\GestureTool.apk");
 
-        //capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
+
         capabilities.setCapability("appPackage", "com.davemac327.gesture.tool");
         capabilities.setCapability("appActivity", "com.davemac327.gesture.tool.GestureBuilderActivity");
-
+        //izinler için
+        //capabilities.setCapability(MobileCapabilityType.NO_RESET, true);
 
 
         // Initialize AndroidDriver
         AndroidDriver driver = new AndroidDriver(new URL("http://127.0.0.1:4723"), capabilities);
+        System.out.println("app yüklendi");
+        //izinler için bu kodlar veya üst satırdaki noReset komutu
+        driver.findElement(By.id("com.android.permissioncontroller:id/continue_button")).click();
+        driver.findElement(By.xpath("//android.widget.Button[@text='Tamam']")).click();
+
+        WebElement homeScreenTitle =driver.findElement(By.id("android:id/title"));
+        Assert.assertTrue(homeScreenTitle.isDisplayed());
+        System.out.println("ana sayfa açıldı");
+
+        WebElement testbutton=driver.findElement(By.id("com.davemac327.gesture.tool:id/testButton"));
+        testbutton.click();
+        System.out.println("test button çalışıyor");
+
+        WebElement testGestureIdTitle =driver.findElement(By.id("android:id/title"));
+        Assert.assertTrue(testGestureIdTitle.isDisplayed());
+        System.out.println("test ana sayfa açıldı");
+
+        //session close
+        //driver.close();
 
 
 
